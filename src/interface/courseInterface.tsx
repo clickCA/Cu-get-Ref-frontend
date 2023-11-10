285350836562c5c7990ecdd30412d9e8a048a413
SorawitThanitt <sorawit.thanit@gmail.com>
2023-11-10 11:37:55 +0700

    ADD COURSE

---
+++ src/interface/courseInterface.tsx
@@ -0,0 +1,15 @@
+export interface CourseProps{
+    courseId: string 
+	courseName: string 
+	courseDescription: string 
+	facultyDepartment: string 
+	academicTerm: string 
+	academicYear: Number  
+	professors: string 
+	prerequisites: string 
+	status: string 
+	curriculumName: string 
+	degreeLevel: string 
+	teachingHours: Number  
+
+}
